7a665f2b1173d01068da676547b3fea4cba28a73
rodrigo11gg <castanedarodrigo609@gmail.com>
2025-05-22 17:54:08 -0500

    ejercicio7

--- SEMANA 1/ejer7.py
+++ SEMANA 1/ejer7.py
@@ -2,3 +2,8 @@
 Escribe un programa que solicite al usuario un número entero y
 determine si es par o impar. resulevelo
 '''
+numero = int(input("Introduce un número entero: "))
+if numero % 2 == 0:
+    print("es un numero par")
+else:
+    print("es un numero impar")
