077085a7a6ef2ce6559ed63cb238196fa2f05fc6
Astry0805 <aipanaquepalacios@gmail.com>
2025-05-22 18:06:39 -0500

    actualizar

--- SEMANA 1/ejer5.py
+++ SEMANA 1/ejer5.py
@@ -7,3 +7,14 @@ estudiante (entre 0 y 100) y le asigne una letra según la siguiente tabla:
 - 60-69: D
 - Menos de 60: F
 '''
+nota= int(input("Ingrese la calificación del estudiante: "))
+if(nota>=90): 
+    print("Tu calificación es A")
+elif(nota>=80):
+    print("Tu calificación es B")
+elif(nota>=70):
+    print("Tu calificación es C")
+elif(nota>=60):
+    print("Tu calificación es D")
+else:
+    print("Tu calificación es F")
