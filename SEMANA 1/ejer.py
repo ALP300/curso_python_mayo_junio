e6375bb7903e8454d6b0fa13dabc52e8ac702df0
ALP300 <aitorpalomino1@gmail.com>
2025-05-22 17:29:48 -0500

    Realizar ejercicios

--- SEMANA 1/ejer.py
+++ SEMANA 1/ejer.py
@@ -2,3 +2,21 @@
 Escribe un programa que solicite al usuario dos números y muestre su
 suma, resta, multiplicación, división, división entera y residuo (módulo).
 '''
+num1= float(input("Por favor ingresa el número 1: "))
+num2= float(input("Por favor ingresa el número 2: "))
+suma= num1+num2
+resta= num1-num2
+multiplicación= num1*num2
+if num2!=0:
+    div= num1/num2
+    div_Entera= num1//num2
+    modulo= num1%num2
+else:
+    print("No se puede dividir por cero")
+
+print(f"La suma es: {suma} ")
+print(f"La resta es: {resta} ")
+print(f"La multiplicación es: {multiplicación} ")
+print(f"La división es: {div} ")
+print(f"La división entera es: {div_Entera} ")
+print(f"El módulo es: {modulo} ")
--- SEMANA 1/ejer2.py
+++ SEMANA 1/ejer2.py
@@ -2,3 +2,11 @@
 Escribe un programa que solicite al usuario un número entero y calcule
 su cuadrado y su cubo.
 '''
+import math
+num1= float(input("Por favor ingresa un número entero: "))
+cuadra_math= math.pow(num1,2)
+raiz= math.sqrt(25)
+cuadra= num1**2
+cubo= num1**3
+print(cuadra_math)
+print(raiz)
