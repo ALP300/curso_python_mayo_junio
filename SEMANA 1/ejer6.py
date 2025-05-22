f4a7c1feb4a7a90a2e5ef94737f437f05867ebc7
javidev21 <ingjaviersalas@gmail.com>
2025-05-22 17:04:10 -0600

    Primer avance

--- SEMANA 1/ejer6.py
+++ SEMANA 1/ejer6.py
@@ -2,3 +2,14 @@
 Escribe un programa que solicite al usuario tres números y determine
 cuál de ellos es el mayor.
 '''
+
+num1= float(input("Por favor ingrese el número 1"))
+num2= float(input("Por favor ingrese el número 2"))
+num3= float(input ("Por favir ingrese el número 3"))
+
+if (num1>num2):
+    return num1
+elif num1>
+    
+
+print(resultado)
