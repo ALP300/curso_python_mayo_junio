af37c7ddd9b3b9033527a7bdd91786f9eb7101b2
ALP300 <aitorpalomino1@gmail.com>
2025-05-22 17:35:16 -0500

    Actualizar proyecto

--- SEMANA 1/ejer2.py
+++ SEMANA 1/ejer2.py
@@ -3,7 +3,7 @@ Escribe un programa que solicite al usuario un número entero y calcule
 su cuadrado y su cubo.
 '''
 import math
-num1= float(input("Por favor ingresa un número entero: "))
+num1= float(input("Por favor ingresa un número entero:  "))
 cuadra_math= math.pow(num1,2)
 raiz= math.sqrt(25)
 cuadra= num1**2
