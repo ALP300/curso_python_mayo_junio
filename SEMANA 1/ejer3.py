3be82a8078a6d9937ebe80eb17e502cff983a3c6
FOX12-D <Julioechevarria200@gmail.com>
2025-05-22 17:56:45 -0500

    actualizar ejercicio 3

--- SEMANA 1/ejer3.py
+++ SEMANA 1/ejer3.py
@@ -2,3 +2,11 @@
 Escribe un programa que lea un número entero y determine si es
 positivo, negativo o cero.
 '''
+num = int(input("Ingresa un numero entero: "))
+if num > 0:
+    print("El numero es positivo")
+elif num < 0:
+    print("El numero es negativo")
+else:
+    print("El numero es cero")
+    
