9dddce690fa1dc634059669db60634f1e4390365
Brapi2 <andre.chufandama@gmail.com>
2025-06-10 16:30:02 -0500

    ponerse al dia  checkpoint

--- SEMANA 1/ejer.py
+++ SEMANA 1/ejer.py
@@ -1,3 +1,4 @@
+"ponerse al dia con el lenguaje de programación Python"
 '''
 Escribe un programa que solicite al usuario dos números y muestre su
 suma, resta, multiplicación, división, división entera y residuo (módulo).
